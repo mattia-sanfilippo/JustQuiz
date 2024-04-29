66fc70474090fd36b4759c13d06fcfa3ebcb9581
Mattia Sanfilippo <me@mattiasanfilippo.dev>
2024-04-29 18:55:20 +0200

    feat: repeat quiz

--- app/src/main/java/com/mattiasanfilippo/justquiz/MainActivity.kt
+++ app/src/main/java/com/mattiasanfilippo/justquiz/MainActivity.kt
@@ -7,17 +7,24 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.tooling.preview.Preview
@@ -28,6 +35,9 @@ import com.mattiasanfilippo.justquiz.model.QuizList
 import com.mattiasanfilippo.justquiz.ui.components.QuizCard
 import com.mattiasanfilippo.justquiz.ui.components.TopAppBar
 import com.mattiasanfilippo.justquiz.ui.theme.AppTheme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import java.io.InputStream
 
 class MainActivity : ComponentActivity() {
@@ -40,9 +50,24 @@ class MainActivity : ComponentActivity() {
         val gson = Gson()
         val quizList = gson.fromJson(json, QuizList::class.java)
 
+        val db = MainApplication.database
+        val completedQuizzes = mutableSetOf<Quiz>()
+
+        CoroutineScope(Dispatchers.IO).launch {
+            val results = db.resultDao().getAll()
+            results.forEach() { result ->
+                val quiz = quizList.quizzes.find { it.id == result.quizId }
+                if (quiz != null) {
+                    completedQuizzes.add(quiz)
+                }
+            }
+
+        }
+
+
         setContent {
             AppTheme {
-                Content(quizList.quizzes, ::onQuizCardClick)
+                ContentWithDialog(quizList.quizzes, completedQuizzes.toList(), ::onQuizCardClick, ::onConfirmRetryQuiz, ::onConfirmWithoutCorrectAnswersQuiz)
             }
         }
     }
@@ -52,6 +77,59 @@ class MainActivity : ComponentActivity() {
         intent.putExtra("QUIZ_ID", quizId)
         startActivity(intent)
     }
+    
+    private fun onConfirmRetryQuiz(quizId: Int) {
+        val intent = Intent(this, QuizActivity::class.java)
+        intent.putExtra("QUIZ_ID", quizId)
+        startActivity(intent)
+    }
+
+    private fun onConfirmWithoutCorrectAnswersQuiz(quizId: Int) {
+        val intent = Intent(this, QuizActivity::class.java)
+        intent.putExtra("QUIZ_ID", quizId)
+        intent.putExtra("ONLY_WRONG_QUESTIONS", true)
+        startActivity(intent)
+    }
+}
+
+@Composable
+fun ContentWithDialog (quizzes: List<Quiz>, completedQuizzes: List<Quiz>, onQuizCardClick: (Int) -> Unit, onConfirmRetryQuiz: (Int) -> Unit, onRetryWithoutCorrectAnswersQuiz: (Int) -> Unit){
+    val showDialog = remember { mutableStateOf(false) }
+    val selectedQuizId = remember { mutableIntStateOf(0) }
+
+    fun onRetryQuizCardClick(quizId: Int) {
+        selectedQuizId.intValue = quizId
+        showDialog.value = true
+    }
+
+    fun onConfirmRetryQuizClick() {
+        showDialog.value = false
+        onConfirmRetryQuiz(selectedQuizId.intValue)
+    }
+
+    fun onRetryWithoutCorrectAnswersQuizClick() {
+        showDialog.value = false
+        onRetryWithoutCorrectAnswersQuiz(selectedQuizId.intValue)
+    }
+
+    if (showDialog.value) {
+        AlertDialog(
+            onDismissRequest = { showDialog.value = false },
+            title = { Text("Retry quiz") },
+            text = { Text("Do you want to retry the quiz with only the questions you answered incorrectly?") },
+            confirmButton = {
+                TextButton(onClick = { onRetryWithoutCorrectAnswersQuizClick() }) {
+                    Text("Yes")
+                }
+            },
+            dismissButton = {
+                TextButton(onClick = { onConfirmRetryQuizClick() }) {
+                    Text("No")
+                }
+            }
+        )
+    }
+    Content(quizzes, completedQuizzes, onQuizCardClick, ::onRetryQuizCardClick)
 }
 
 @Preview
@@ -64,36 +142,61 @@ fun PreviewMainActivity() {
                 Quiz(2, "Quiz Name 2", 20),
                 Quiz(3, "Quiz Name 3", 30),
             ),
-            onQuizCardClick = { }
+            completedQuizzes = listOf(
+                Quiz(4, "Quiz Name 4", 40),
+                Quiz(5, "Quiz Name 5", 50),
+                Quiz(6, "Quiz Name 6", 60),
+            ),
+            onQuizCardClick = { },
+            onRetryQuizCardClick = { }
         )
     }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Content(quizzes: List<Quiz>, onQuizCardClick: (Int) -> Unit) {
+fun Content(quizzes: List<Quiz>, completedQuizzes: List<Quiz>, onQuizCardClick: (Int) -> Unit, onRetryQuizCardClick: (Int) -> Unit) {
     val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(rememberTopAppBarState())
     Scaffold(
         modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
         topBar = { TopAppBar(scrollBehavior = scrollBehavior) },
     ) {
         innerPadding ->
-        ScrollContent(innerPadding, quizzes, onQuizCardClick)
+        ScrollContent(innerPadding, quizzes, completedQuizzes, onQuizCardClick, onRetryQuizCardClick)
     }
 }
 
 @Composable
-fun ScrollContent(innerPadding: PaddingValues, quizzes: List<Quiz>, onQuizCardClick: (Int) -> Unit) {
+fun ScrollContent(innerPadding: PaddingValues, quizzes: List<Quiz>, completedQuizzes: List<Quiz>, onQuizCardClick: (Int) -> Unit, onRetryQuizCardClick: (Int) -> Unit) {
     Column (modifier = Modifier.padding(innerPadding)) {
-        Text(text = "Quizzes", color = MaterialTheme.colorScheme.primary, fontSize = MaterialTheme.typography.titleLarge.fontSize)
-        LazyVerticalGrid(columns = GridCells.Adaptive(156.dp), contentPadding = PaddingValues(
-            start = 16.dp,
-            end = 16.dp
-        ), verticalArrangement = Arrangement.spacedBy(16.dp), horizontalArrangement = Arrangement.spacedBy(16.dp)) {
-            items(quizzes) { quiz ->
-                QuizCard(quiz = quiz, onClick = { onQuizCardClick(quiz.id) })
-            }
+        CompletedQuizzesGrid(completedQuizzes, onRetryQuizCardClick)
+        Spacer(modifier = Modifier.height(16.dp))
+        NewQuizGrid(quizzes, onQuizCardClick)
+    }
+}
+
+@Composable
+fun NewQuizGrid(quizzes: List<Quiz>, onQuizCardClick: (Int) -> Unit) {
+    Text(text = "All quizzes", color = MaterialTheme.colorScheme.primary, fontSize = MaterialTheme.typography.titleLarge.fontSize)
+    LazyVerticalGrid(columns = GridCells.Adaptive(156.dp), contentPadding = PaddingValues(
+        start = 16.dp,
+        end = 16.dp
+    ), verticalArrangement = Arrangement.spacedBy(16.dp), horizontalArrangement = Arrangement.spacedBy(16.dp)) {
+        items(quizzes) { quiz ->
+            QuizCard(quiz = quiz, onClick = { onQuizCardClick(quiz.id) })
         }
     }
 }
 
+@Composable
+fun CompletedQuizzesGrid(completedQuizzes: List<Quiz>, onQuizCardClick: (Int) -> Unit) {
+    Text(text = "Practice again", color = MaterialTheme.colorScheme.primary, fontSize = MaterialTheme.typography.titleLarge.fontSize)
+    LazyVerticalGrid(columns = GridCells.Adaptive(156.dp), contentPadding = PaddingValues(
+        start = 16.dp,
+        end = 16.dp
+    ), verticalArrangement = Arrangement.spacedBy(16.dp), horizontalArrangement = Arrangement.spacedBy(16.dp)) {
+        items(completedQuizzes) { quiz ->
+            QuizCard(quiz = quiz, onClick = { onQuizCardClick(quiz.id) })
+        }
+    }
+}
